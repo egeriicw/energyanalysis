77d08e13683a5fcf978d91bb05334d0a506f1754
Bill Eger <egeriicw@gmail.com>
2018-06-15 13:16:45 -0400

    Setting up the package

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='lean_energy_analysis',
+    version='0.1.0',
+    author='Bill Eger',
+    author_email='egeriicw@gmail.com',
+    packages=['lean_energy_analysis', 'lean_energy_analysis.test'],
+    scripts=['',''],
+    url='http://pypi.python.org/pypi/lean_energy_analysis/',
+    license='LICENSE',
+    description='Lean Energy Analysis Algorithms.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        " >= ",
+        " == ",
+    ],
+)
